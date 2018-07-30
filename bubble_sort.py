573f01bc51b12449c12c8e1bc80ac55d24d3bb4b
Mike Jaren Yap <mjaren.yap@gmail.com>
2018-07-30 11:15:31 +0800

    Initial commit with an bubble sort implementation

---
+++ bubble_sort.py
@@ -0,0 +1,10 @@
+def sort(arr):
+	for i in range(len(arr)):
+		for j in range(i, len(arr)):
+			if arr[i] > arr[j]:
+				arr[i], arr[j] = arr[j], arr[i]
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+sort(arr)
+
+print(arr)
