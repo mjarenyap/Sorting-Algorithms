7abddd28a674812e7d1146598a65d4dd2b8b90d6
Mike Jaren Yap <mjaren.yap@gmail.com>
2018-08-21 12:35:14 +0800

    Updated sorting algos

--- counting_sort.py
+++ counting_sort.py
@@ -5,12 +5,12 @@ def sort(arr):
 
 	# place each element in arr to the proper indexes
 	for i in arr: c[i] += 1
-	# print("C Array:", c) # comment this for time testing
+	print("C Array:", c) # comment this for time testing
 
 	# add the previous counts
 	for i in range(1, len(c)-1):
 		c[i+1] = c[i] + c[i+1]
-	# print("C Array:", c) # comment this for time testing
+	print("C Array:", c) # comment this for time testing
 
 	# make an array with the same length as arr
 	b = [-1] * len(arr)
@@ -18,15 +18,14 @@ def sort(arr):
 		b[c[i] - 1] = i
 		c[i] -= 1
 
-	# print("B Array:", b) # comment this for time testing
+	print("B Array:", b) # comment this for time testing
 
 # main
 # sample input: [64, 34, 25, 12, 22, 11, 90]
-# arr = input().split(" ")
-arr = [64, 34, 25, 12, 22, 11, 90]
+arr = input().split(", ")
 for i in range(len(arr)): arr[i] = int(arr[i]) 
 start_time = time.time()
 sort(arr)
 end_time = time.time()
 
-print("Execution Time: ", end_time - start_time)
+# print("Execution Time: ", end_time - start_time)
